0194a08ec3738742fb07e8bee5e5f0fae599852f
koyna khare <92796533+koynakhare@users.noreply.github.com>
2023-07-01 17:54:04 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import axios from 'axios';
-import Table from './pages/table';
+import axios from "axios";
+import Table from "./pages/table";
 import { useState, useEffect } from "react";
-import Updatedata from './component/AddProduct/addProduct';
-import{ Switch,Route } from "react-router-dom"
+import Updatedata from "./component/AddProduct/addProduct";
+import { Switch, Route } from "react-router-dom";
 
-import Form from './component/AddProduct/addProduct';
-
-
-
-
+import Form from "./component/AddProduct/addProduct";
 
 function App() {
- 
-return (
+  return (
     <>
+      <h1>hiii</h1>
       <Switch>
-        
-      <Route exact path="/update">{<Updatedata/>}</Route>
-      <Route exact path="/" >{<Table />}</Route>
-      {/* <Route exact path="/" >{<InitialSort />}</Route> */}
-      <Route exact path="/form" >{<Form />}</Route>
-     </Switch>
-   
+        <Route exact path="/update">
+          {<Updatedata />}
+        </Route>
+        <Route exact path="/">
+          {<Table />}
+        </Route>
+        {/* <Route exact path="/" >{<InitialSort />}</Route> */}
+        <Route exact path="/form">
+          {<Form />}
+        </Route>
+      </Switch>
     </>
-    
   );
 }
 
-export default App; 
+export default App;
